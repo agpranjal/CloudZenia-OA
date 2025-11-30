11557ac5d254f73e744d3b087d4a515c9844b24d
Pranjal Agrawal <pranjal.ag.1999@gmail.com>
2025-12-01 00:35:09 +0530

    create eip + nat gateway

--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -87,9 +87,35 @@ resource "aws_route_table" "public" {
   })
 }
 
+# Elastic IP for NAT Gateway
+resource "aws_eip" "nat" {
+  domain = "vpc"
+
+  tags = merge(var.tags, {
+    Name = "${var.name_prefix}-nat-eip"
+  })
+}
+
+# NAT Gateway for private subnets
+resource "aws_nat_gateway" "main" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.public_1.id
+
+  tags = merge(var.tags, {
+    Name = "${var.name_prefix}-nat-gateway"
+  })
+
+  depends_on = [aws_internet_gateway.main]
+}
+
 # Private Route Table
 resource "aws_route_table" "private" {
   vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.main.id
+  }
 
   tags = merge(var.tags, {
     Name = "${var.name_prefix}-private-rt"
--- terraform/modules/vpc/outputs.tf
+++ terraform/modules/vpc/outputs.tf
@@ -53,3 +53,18 @@ output "private_route_table_id" {
   value       = aws_route_table.private.id
 }
 
+output "nat_gateway_id" {
+  description = "ID of the NAT Gateway"
+  value       = aws_nat_gateway.main.id
+}
+
+output "nat_gateway_public_ip" {
+  description = "Public IP address of the NAT Gateway (Elastic IP)"
+  value       = aws_eip.nat.public_ip
+}
+
+output "nat_gateway_eip_id" {
+  description = "ID of the Elastic IP associated with the NAT Gateway"
+  value       = aws_eip.nat.id
+}
+
